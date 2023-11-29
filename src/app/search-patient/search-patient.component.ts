a11036a3541c829a05f13cdd5b706b8fd26a2d52
Moula imen <imenmoula8@gmail.com>
2023-11-29 07:28:48 +0100

    search

--- src/app/search-patient/search-patient.component.ts
+++ src/app/search-patient/search-patient.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { patient } from '../model/patient.model';
 import { PatientService } from '../services/patient.service';
+import { Groupe } from '../model/groupe.model';
 @Component({
   selector: 'app-search-patient',
   templateUrl: './search-patient.component.html',
@@ -8,7 +9,9 @@ import { PatientService } from '../services/patient.service';
 })
 export class SearchPatientComponent {
   list_pat:patient[];
-
+  list_grp!:Groupe[];
+  critere ='nom';
+  gripidRech !: number;
   nomPat:string='';
   constructor(private patientserv:PatientService) {
     this.list_pat = patientserv.listPatients()
@@ -16,7 +19,18 @@ export class SearchPatientComponent {
 
     chercherParNom (name : string){
       this.list_pat = this.patientserv.listPatients()
-      this.list_pat = this.list_pat.filter(p=> p.nom==name)
+      this.list_pat = this.list_pat.filter(p=> p.nom.toLocaleLowerCase().includes(name.toLocaleLowerCase()))
       }
+      ///chercher par groupe 
+      chercherParGrp() {
+        this.list_pat = this.patientserv.listPatients();
+        if (this.gripidRech != 0) {
+          this.list_pat = this.list_pat.filter(p => p.grp.idGrp == this.gripidRech)
+        }
+      }
+
+      ngOnInit(){
+        this.list_grp = this.patientserv.listeGroupe()
+        }
 }
 
